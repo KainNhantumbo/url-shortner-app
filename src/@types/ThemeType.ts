fdb39e8168dd4a869bffa1a619bdb5b745e94318
Kain Nhantumbo <nhantumbok@gmail.com>
2022-10-02 14:18:35 +0200

    feat: added theme types

---
+++ src/@types/ThemeType.ts
@@ -0,0 +1,11 @@
+export type ThemeType = {
+	primary: string;
+	secondary: string;
+	accent: string;
+	font: string;
+	neutral: string;
+	background: string;
+	foreground: string;
+	alternative_a: string;
+	alternative_b: string;
+};
