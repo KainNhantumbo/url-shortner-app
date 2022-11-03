ccd71caf4521b97f5f95210ad01074fc5ad7518f
Kain Nhantumbo <nhantumbok@gmail.com>
2022-11-03 14:09:51 +0200

    feat: added base url of the api production

--- src/service/api-client.ts
+++ src/service/api-client.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8500/';
+// const BASE_URL = 'http://localhost:8500/';
+const BASE_URL = 'https://amethyst-quail-coat.cyclic.app';
 
 const apiClient = axios.create({ baseURL: BASE_URL });
 apiClient.defaults.headers.common['Accept'] = 'application/json';
